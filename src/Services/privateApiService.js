1afb7421187ad2a21a3d77c4bac104e8d509361b
Gabarron <O000863@bf.arg.igrupobbva>
2022-08-19 15:39:45 -0300

    feat: post function was created

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,14 +2,25 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        // Group: 01            //Aqui va el ID del equipo!!
     }
 }
+const apiUrl= "https://ongapi.alkemy.org/api/";
+let url = "+";
 
-const Get = () => {
+function Get() {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
 }
 
-export default Get
+const Post = async (apiUrl, body) =>{
+    try{
+        const response = await axios.post(apiUrl, url,body)
+        return response
+    } catch (error) {
+        return error
+    }
+};
+
+export default {Get, Post};
