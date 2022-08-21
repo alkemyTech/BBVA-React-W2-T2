9ac7ae1e6f959e05a1390d20bee89198d8ff76f0
margaritasing <libiamfreitesm@gmail.com>
2022-08-21 15:23:11 -0300

    feature: agrego el endpoint global para el delete

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,31 @@
 import axios from 'axios';
 
+
+
 const config = {
     headers: {
         Group: 01                //Aqui va el ID del equipo!!
     }
 }
 
+const apiUrl = "https://ongapi.alkemy.org/api";
+let url = "";
+
 const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
+    .then(res => console.log(res))    
     .catch(err => console.log(err))
 }
 
-export default Get
+const Delete = async (apiUrl) =>{
+
+    try {
+        const response = await axios.delete(apiUrl, url, config);
+        return response;
+    } catch (error) {
+        return error
+    }
+
+}
+
+export default { Get, Delete}
