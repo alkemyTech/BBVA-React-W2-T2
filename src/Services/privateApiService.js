ef3c2fbf35c96e125ffb27820cec0103bdd3b0cb
CarlaDevincenzi <anabellacarla2301@gmail.com>
2022-08-22 12:13:48 -0300

    fix: correccion de metodo Put en privateApiService.js

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const api_url = "https://ongapi.alkemy.org/api/";
-let url = "";
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 54321                //Aqui va el ID del equipo!!
     }
 }
 
@@ -15,14 +14,14 @@ const Get = () => {
     .catch(err => console.log(err))
 }
 
-const Put = (api_url, body) =>{
+const Put = (url, body) =>{
+
+    let final_url = api_url + url;
     
-        axios.put(api_url, url, body)
+        axios.put(final_url, body)
             .then(response => console.log(response))
             .catch(error => console.log(error));   
         
 }
-          
-
 
 export default {Get, Put}
