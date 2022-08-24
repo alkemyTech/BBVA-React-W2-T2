ee0791ef6b6549964d3fdbf9db787d4966fe1c0b
margaritasing <libiamfreitesm@gmail.com>
2022-08-24 12:46:39 -0300

    fix: agrego script para evitar conflictos en main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
