7686b77c185f84580ae83e3f042c21d9204a866d
margaritasing <libiamfreitesm@gmail.com>
2022-08-24 12:38:07 -0300

    feature:agrego metodos http al news, tambien la libreria sweet alert para gestionar errore

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
@@ -22,7 +20,8 @@ function App() {
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
-          <Route path="/create-news" component={NewsForm} />
+          <Route path="/news" component={NewsForm} />
+          <Route path="news/:id" component={NewsForm}/>
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
@@ -33,51 +32,7 @@ function App() {
         </Switch>
       </BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+     
     </div>
     </>
   );
---
+++ src/Components/Alert/Alert.js
@@ -0,0 +1,12 @@
+import swal from 'sweetalert';
+
+
+
+// And with a third argument, you can add an icon to your alert! There are 4 predefined ones: "warning", "error", "success" and "info".
+const Alert = (title, message, type) =>{
+    swal(title, message, type);
+
+}
+
+
+export default Alert
