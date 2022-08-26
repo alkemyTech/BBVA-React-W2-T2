d75c0bf7e38a4a407eb445b473d48fedde265fa1
GuilleEpayco <guillermo.voleger@payco.co>
2022-08-26 09:52:47 -0300

    fix: adding /

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,51 +1,47 @@
-import axios from 'axios';
+import axios from "axios";
 
 const config = {
-    headers: {
-        Group: 54321                
-    }
-}
+  headers: {
+    Group: 54321,
+  },
+};
 const apiUrl = "https://ongapi.alkemy.org/api/";
 
-const Get = (url) => {
-    let urlFinal = apiUrl + url;
-    
-    axios.get(urlFinal)
-        .then(res => console.log(res))
-        .catch(err => console.log(err));
-    
-}
+const Get = async (url) => {
+  let urlFinal = apiUrl + url;
 
-const Delete = async (url) =>{
+  return await axios
+    .get(urlFinal)
+    .then((res) => res.data.data)
+    .catch((err) => err);
+};
 
-    let urlFinal = apiUrl + url
+const Delete = async (url) => {
+  let urlFinal = apiUrl + url;
 
-    try {
-        return await axios.delete(urlFinal);
-    } catch (error) {
-        return error
-    }
+  try {
+    return await axios.delete(urlFinal);
+  } catch (error) {
+    return error;
+  }
+};
 
-}
+const Put = async (url, body) => {
+  let urlFinal = apiUrl + url;
 
-const Put = (url, body) =>{
+  return await axios
+    .put(urlFinal, body)
+    .then((response) => response.data)
+    .catch((error) => error);
+};
 
-     let urlFinal = apiUrl + url;
-    
-        axios.put(urlFinal, body)
-            .then(response => console.log(response))
-            .catch(error => console.log(error)); 
-        
-}
-
-const Post = (url, body) =>{
-    let urlFinal = apiUrl + url;
-    axios.post(urlFinal, body)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
- }
-
+const Post = async (url, body) => {
+  let urlFinal = apiUrl + url;
+  return await axios
+    .post(urlFinal, body)
+    .then((res) => res.data)
+    .catch((err) => err);
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {Get, Put, Delete, Post}
-
+export default { Get, Put, Delete, Post };
