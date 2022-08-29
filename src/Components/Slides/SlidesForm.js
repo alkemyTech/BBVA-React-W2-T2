2ed392a3cddcd147577946dc3f168d8b24366ece
CarlaDevincenzi <anabellacarla2301@gmail.com>
2022-08-29 02:49:23 -0300

    feat: Slides: formulario creacion y edicion

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,107 +1,140 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import  ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import apiPrivate from '../../Services/privateApiService'; 
 import '../FormStyles.css';
 
-function SlidesForm (props) {
 
-   /*const props = {
-        id: 1411,
-        name: "Juntos somos más",
-      description: "<p>Sé parte de nuestra comunidad y cambiemos el mundo.</p>",
-      image: "http://ongapi.alkemy.org/storage/2sjocle1Nz.png",
-      order: 1
-      
-    }*/
-    
+function SlidesForm () {
+
+    let { id } = useParams();
+    //let id = 1413; // para probar la edicion de formulario
+    const [formSubmited, setFormSubmited] = useState(false);
+    const [errors, setErrors] = useState({});
     const [initialValues, setInitialValues] = useState({
         name: '',
         description: '',         
-        image: ''        
+        image: '',
+        order: '' // es auto-incremental
     })               
     
-    if(props.name){
-        setInitialValues({...initialValues, name: props.name, description: props.description, image: props.image}) 
-                
-    }
-    
-    
-    const validateExtension = (image_src) =>{
-        const allowedExtensions = /(.jpg|.png)/;
-
-        return allowedExtensions.test(image_src);
-    }
-
-    const handleChange = (e) => {        
+    useEffect(async () =>{
+        let slideData = '';
         
-        switch (e.target.name) {
-            case 'name':                
-                if(e.target.value.lenght < 4){
-                    alert("La longitud no puede ser inferior a 4 caracteres");
-                }else{
-                    setInitialValues({...initialValues, name: e.target.value});
-                }
-                break;
-            case 'description':
-                if(e.target.value === ''){
-                    alert("Debe incluir una descripcion");
-                }else{
-                    setInitialValues({...initialValues, description: e.target.value});
-                }
-                break;
-            case 'image':
-                if(validateExtension(e.target.value)){
-                    setInitialValues({...initialValues, image: e.target.value});
-                }else{
-                    alert("Formato de imagen invalido!");
-                }                
-                break;
-            default:
-                break;
+        if(id){ // si llega un id, busca ese slide para modificarlo, mostrando los valores iniciales con la data del slide
+            slideData = await apiPrivate.Get(`slides/${id}`);
+            setInitialValues({...initialValues, 
+                                name: slideData.name, 
+                                description: slideData.description,
+                                image: slideData.image,                               
+                                order: slideData.order});
+        }else{ // si no hay id, se crea un slide desde 0, pero se recupera el ultimo numero de order cargado, para setear el nuevo al valor siguiente
+            slideData = await apiPrivate.Get('slides');
+            if(slideData.length > 0){
+                let final = slideData.length - 1; // busco la ultima posicion del array
+                let lastOrder = slideData[final].order; // accedo al ultimo order en el array
+                setInitialValues({...initialValues, order: ++lastOrder}); // seteo el nuevo order al valor siguiente
+            }
+        }
+    }, []);
+         
+    
+    const handleChange = (e) => {       
+        if(e.target.name === 'name'){
+            setInitialValues({...initialValues, name: e.target.value});
+        }        
+              
+        if(e.target.name === 'image'){
+            setInitialValues({...initialValues, image: e.target.value});
         }
     }
 
+    const validateFields = () => {
+        
+        if(initialValues.name === '' || initialValues.name.length < 4 ){
+            setErrors({...errors, name:"El campo 'Titulo' debe contener al menos 4 caracteres"});
+            return false;
+        }else{setErrors({...errors, name:""})}
+        
+        if(initialValues.description === ''){
+            setErrors({...errors, description:"Debe incluir una 'Descripcion'"});
+            return false;            
+        }else{setErrors({...errors, description:""})}
+        
+        if(initialValues.image === ''){
+            setErrors({...errors, image: "Debe incluir una imagen"});
+            return false;
+        }else{setErrors({...errors, image: ""})}
+       
+        return true;
+    };
+
+    
+   
     const handleSubmit = (e) =>{
         e.preventDefault();        
-        let id = props.id;
-       
-        if(id){            
-            apiPrivate.Put(`slides/${id}`, initialValues);
-        }else{
-            
-            apiPrivate.Post('slides', initialValues);
-        }
-          
-        console.log(initialValues);
+       let validate = validateFields();
+
+        if(validate){
+            if(id){            
+                apiPrivate.Put(`slides/${id}`, initialValues);
+                console.log("Formulario editado");
+            }else{           
+                apiPrivate.Post('slides', initialValues);
+                console.log("Formulario creado");
+            }
+            setFormSubmited(true);
+            setTimeout(() => setFormSubmited(false), 5000); 
+            console.log(initialValues);
+        }        
     }
     
     
 
     return (
         <form className="form-container" onSubmit={ handleSubmit }>
-            <div><span>Order: { initialValues.order }</span></div> <br />
+            <h3>FORMULARIO CREACION/EDICION DE SLIDE</h3>
+            <div className="input-field"><b>Order N°: { initialValues.order }</b></div>
             
-            <input className="input-field" type="text" required name="name" value={ initialValues.name } onChange={ handleChange } placeholder="Titulo"></input>
-            
+            <input className="input-field" type="text" name="name" value={ initialValues.name } 
+                    onChange={ handleChange } placeholder="Titulo" />
+            <div>
+                <p className="error">{ errors.name }</p>
+            </div>
             <div >
                 <CKEditor 
                     editor={ ClassicEditor }
                     data={ initialValues.description }
                     name="description"
-                                        
+                                       
                     onChange={ ( event, editor ) => {
                         const data = editor.getData();
-                         initialValues.description = ((statePrev) => ({...statePrev, description:data}));                        
+                         setInitialValues((statePrev) => ({...statePrev, description:data}));                        
                     }}
                 />
-           </div>            
-            
-            <input type="file" required name="image" value={ initialValues.image } onChange={ handleChange }></input>            
-                       
+                <p className="error">{ errors.description }</p>
+           </div>
+                      
+           <div> 
+              {(!id) ?  /*si es de creacion se deja el value, si es edicion no se puede traer el value al input file, asi que lo dejo vacio y pongo la ruta en un parrafo */    
+                <input type="file" accept="image/png, image/jpeg" name="image" 
+                    value={ initialValues.image } onChange={ handleChange } /> :
+                <> 
+                    <input type="file" accept="image/png, image/jpeg" name="image" 
+                        value='' onChange={ handleChange } />
+                    <p className="image-scr">{ initialValues.image }</p>
+                </> 
+                }
+                
+                <p className="error">{ errors.image }</p>
+                <br />             
+                { initialValues.image && <img scr={ initialValues.image } className="myImg" alt="imagen" /> }         
+            </div>
             <button className="submit-btn" type="submit">Enviar</button>
 
-            <img scr={ initialValues.image } className="myImg" alt="imagen" />
+            {formSubmited && <p className="success">Formulario creado/modificado exitosamente!</p>}
+            
         </form>
     );
 }
